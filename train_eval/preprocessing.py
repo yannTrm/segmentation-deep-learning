b078a61333a0f9b96633ff427a1caba2b98c1933
Yann Terrom <87117905+yannTrm@users.noreply.github.com>
2025-05-28 12:50:13 +0200

    Update preprocessing.py

--- train_eval/preprocessing.py
+++ train_eval/preprocessing.py
@@ -1,23 +1,21 @@
 import albumentations as A
 
-
-def get_portrait_validation_augmentation():
-    """Add paddings to make portrait image shape divisible by 32"""
-    test_transform = [
-        A.PadIfNeeded(
-            min_height=1088,  # 1080 + 8 (multiple of 32)
-            min_width=608,   
-            always_apply=True,
-            border_mode=0,    
-        ),
-    ]
-    return A.Compose(test_transform)
+def _calculate_scaled_size(original_size, scale_factor=1.0):
+    """Calcule la taille après scaling en s'assurant qu'elle est divisible par 32"""
+    scaled_h = int(original_size[0] * scale_factor)
+    scaled_w = int(original_size[1] * scale_factor)
+    scaled_h = (scaled_h // 32) * 32
+    scaled_w = (scaled_w // 32) * 32
+    return scaled_h, scaled_w
 
 
-def get_landscape_augmentation():
+def get_portrait_augmentation(scale_factor=1.0, grayscale=False):
+    original_size = (1080, 608)  # (height, width)
+    scaled_h, scaled_w = _calculate_scaled_size(original_size, scale_factor)
+    
     transform = [
-        A.Resize(height=1080, width=1920, always_apply=True),  
-        A.PadIfNeeded(min_height=1080, min_width=1920, always_apply=True),  
+        A.ToGray(p=1.0) if grayscale else A.NoOp(),
+        A.Resize(height=scaled_h, width=scaled_w, always_apply=True),
         A.ShiftScaleRotate(scale_limit=0.1, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0),
         A.GaussNoise(p=0.2),
         A.Perspective(p=0.5),
@@ -37,17 +35,58 @@ def get_landscape_augmentation():
             ],
             p=0.9,
         ),
+        A.PadIfNeeded(min_height=scaled_h, min_width=scaled_w, always_apply=True),
     ]
     return A.Compose(transform)
 
-def get_landscape_validation_augmentation():
-    """Add paddings to make landscape image shape divisible by 32"""
+def get_portrait_validation_augmentation(scale_factor=1.0, grayscale=False):
+    original_size = (1080, 608)  # (height, width)
+    scaled_h, scaled_w = _calculate_scaled_size(original_size, scale_factor)
+    scaled_h = ((scaled_h + 33) // 32) * 32
+    scaled_w = ((scaled_w + 33) // 32) * 32
+    
     test_transform = [
-        A.PadIfNeeded(
-            min_height=1088,  
-            min_width=1920,   
-            always_apply=True,
-            border_mode=0, 
+        A.ToGray(p=1.0) if grayscale else A.NoOp(),
+        A.Resize(height=scaled_h, width=scaled_w, always_apply=True),
+    ]
+    return A.Compose(test_transform)
+
+def get_landscape_augmentation(scale_factor=1.0):
+    original_size = (1080, 1920)  # (height, width)
+    scaled_h, scaled_w = _calculate_scaled_size(original_size, scale_factor)
+    
+    transform = [
+        A.Resize(height=scaled_h, width=scaled_w, always_apply=True),
+        A.GaussNoise(p=0.2),
+        A.Perspective(p=0.5),
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightnessContrast(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
         ),
+        A.OneOf(
+            [
+                A.Sharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+        A.PadIfNeeded(min_height=scaled_h, min_width=scaled_w, always_apply=True, border_mode=0),
+
+    ]
+    return A.Compose(transform)
+
+def get_landscape_validation_augmentation(scale_factor=1.0):
+    original_size = (1080, 1920)  # (height, width)
+    scaled_h, scaled_w = _calculate_scaled_size(original_size, scale_factor)
+    scaled_h = ((scaled_h + 33) // 32) * 32
+    scaled_w = ((scaled_w + 33) // 32) * 32
+    
+    test_transform = [
+    A.Resize(height=scaled_h, width=scaled_w, always_apply=True),
     ]
     return A.Compose(test_transform)
